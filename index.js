a64a0b32e72e639444f9216f3b10400ec6185833
Kurdii <mohammad.m.alkurdi@gmail.com>
2024-03-10 16:39:59 +0300

    last commit

--- index.js
+++ index.js
@@ -19,11 +19,21 @@ $.fn.jQuerySimpleCounter = function( options ) {
         complete: settings.complete
     });
 };
-
-
 $('#number1').jQuerySimpleCounter({end: 1240,duration: 3200});
 $('#number2').jQuerySimpleCounter({end: 2000,duration: 3500});
 $('#number3').jQuerySimpleCounter({end: 1000,duration: 3000});
+
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
 
 
 
