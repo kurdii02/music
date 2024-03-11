1a66b2153b995bd82ad912de4061b519e5e47577
Kurdii <mohammad.m.alkurdi@gmail.com>
2024-03-11 21:50:32 +0300

    last commit

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-function simpleCounter(element, end, duration) {
+function simpleCounter(element, endValue, duration) {
     let start = 0;
-    let range = end - start;
+    let range = endValue - start;
     let current = start;
-    let increment = end > start ? 1 : -1;
+    let increment = endValue > start ? 1 : -1;
     let stepTime = Math.abs(Math.floor(duration / range));
-    let timer;
 
     function updateCounter() {
         current += increment;
-        element.textContent = current+" +";
-        if (current == end) {
-            clearInterval(timer);
+        element.textContent = current + "+";
+        if (current !== endValue) {
+            setTimeout(updateCounter, stepTime);
         }
     }
 
-    timer = setInterval(updateCounter, stepTime);
     updateCounter();
 }
 
-let number1 = document.getElementById('number1');
-let number2 = document.getElementById('number2');
-let number3 = document.getElementById('number3');
+simpleCounter(document.getElementById('number1'), 1240, 4000);
+simpleCounter(document.getElementById('number2'), 1500, 2000); 
+simpleCounter(document.getElementById('number3'), 1000, 4000);
 
-simpleCounter(number1, 1240, 2200);
-simpleCounter(number2, 2000, 1000);
-simpleCounter(number3, 1000, 2400);
+
+
 
 function myFunction() {
     var x = document.getElementById("myLinks");
