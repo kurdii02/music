ea603edde423fc10b48c29d4e7054d77f39eee2b
Kurdii <mohammad.m.alkurdi@gmail.com>
2024-03-11 20:37:49 +0300

    last commit

--- about_counter.js
+++ about_counter.js
@@ -52,3 +52,24 @@ $('#partnersSlider').slick({
     }
   ]
 });
+
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  var icon = document.getElementById("icon");
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    icon.classList.remove("clicked"); 
+
+
+  } else {
+    x.style.display = "block";
+    icon.classList.add("clicked"); 
+
+
+  }
+}
+
+
+document.getElementById("icon").addEventListener("click", myFunction,true);
