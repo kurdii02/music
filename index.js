38c4b650a0754c21d82311848a369210187a876c
Kurdii <mohammad.m.alkurdi@gmail.com>
2024-03-11 21:28:40 +0300

    last commit

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-jQuerySimpleCounter = function( options ) {
-    var settings = $.extend({
-        start:  0,
-        end:    100,
-        easing: 'swing',
-        duration: 400,
-        complete: ''
-    }, options );
+function simpleCounter(element, end, duration) {
+    let start = 0;
+    let range = end - start;
+    let current = start;
+    let increment = end > start ? 1 : -1;
+    let stepTime = Math.abs(Math.floor(duration / range));
+    let timer;
 
-    var thisElement = $(this);
+    function updateCounter() {
+        current += increment;
+        element.textContent = current+" +";
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }
 
-    $({count: settings.start}).animate({count: settings.end}, {
-        duration: settings.duration,
-        easing: settings.easing,
-        step: function() {
-            var mathCount = Math.ceil(this.count);
-            thisElement.text(mathCount+"+");
-        },
-        complete: settings.complete
-    });
-};
-$('#number1').jQuerySimpleCounter({end: 1240,duration: 3200});
-$('#number2').jQuerySimpleCounter({end: 2000,duration: 3500});
-$('#number3').jQuerySimpleCounter({end: 1000,duration: 3000});
+    timer = setInterval(updateCounter, stepTime);
+    updateCounter();
+}
 
+let number1 = document.getElementById('number1');
+let number2 = document.getElementById('number2');
+let number3 = document.getElementById('number3');
+
+simpleCounter(number1, 1240, 2200);
+simpleCounter(number2, 2000, 1000);
+simpleCounter(number3, 1000, 2400);
 
 function myFunction() {
     var x = document.getElementById("myLinks");
